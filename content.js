4829039bfcbe05bd6504f92381bcb2c682847fa8
Alexey <lexalord95@mail.ru>
2019-02-13 22:41:08 +0300

    all lock

--- content.js
+++ content.js
@@ -18,29 +18,54 @@ chrome.runtime.onMessage.addListener(request=>{
 
 // отлавливаем когда есть посты, собираем все строки и отправляем их на сервер предварительно обрав лишнее. записываем все посты по номерам что бы помнить если что какой из них нужно скрыть. скрывать посты посредством скрытия всего поста и замены его на псевдо объект который отрисует свою верстку на его месте с кнопкой раскрыть. функция раскрытия будет этот номер или даже просто этот объект поста убирать нашу верстку и возвращать исходный стиль
 
-var GLOBAL_DATA = {}
+var GLOBAL_DATA = {old_text:[]}
 
 function action(){
+	var arr_send = []
 	$('.js-stream-item').each((index,item)=>{
 
 		var data = $(item).find('.content')[0]
 		// var header = $(data).find('.stream-item-header')[0]
 		var content = $(data).find('.js-tweet-text-container')[0]
 		// var footer = $(data).find('.ProfileTweet-actionList')[0]
-
-		// console.log($(content).text())
-
-		//var post = $('.js-stream-item')[1]
-		//$(post).wrap('<div id="custom" style="opacity:0.2"></div>'); // обертка целевого элемента в свой собственный
-		// $('#custom').css({"opacity":'1'})
+		arr_send.push($(content).text())
+		GLOBAL_DATA.old_text.push($(content).html())
+		$(content).html(`
+			<a class="unlock" data-index="`+index+`">unlock</a>
+		`)
 
 	})
 
+$(document).on('click', '.unlock', function(e){
+	e.preventDefault()
+	$(this).parent().html(GLOBAL_DATA.old_text[$(this).attr('data-index')])
+});
+
+
+
+// $('.unlock').on('click',function(){
+
+// 	$(this).parent().html(old_text)
+// 	// console.log(this,'unlock')
+// })
+
+
+
 }
 
+
+
 function send(mess){
-	var data = {mess:mess}
-	console.log(data)
+	var out = []
+	mess.forEach(item=>{
+		if(rand(0,1)){
+			out.push(true)
+		}else{
+			out.push(false)
+		}
+	})
+	// var data = {mess:mess}
+	// console.log(data)
 	// $.ajax({
 	// 	url:'',
 	// 	data:data,
@@ -52,6 +77,7 @@ function send(mess){
 	// 		console.log(err)
 	// 	}
 	// })
+	return out
 }
 
 
@@ -59,3 +85,7 @@ function send(mess){
 
 
 
+function rand(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
