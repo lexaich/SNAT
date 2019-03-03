b830210c96a05854bfc3d6ecf92ea0e36d8d43c8
Anatoli Belchikov <belchikov1996@gmail.com>
2019-03-04 01:59:42 +0300

    Structure prepared for refactoring

--- sites/pikabu.js
+++ sites/pikabu.js
@@ -1,64 +1,17 @@
+// TODO make abstraction ToxicSorter for such classes
 
-// console.log('dfgdfg')
-window.onload = init
+var oldUrl = null
+var oldCommentsNumber = 0
 
-
-function init(){
-  var place = $('.comments__container')
-  var childrens = $(place).children('.comment')
-
-
-  
-// console.log(place)
-// console.log(childrens)
-generate(childrens)
-
-var response = sendDev(request)
-setToxicity(response)
-// console.log(response)
-}
-
-var request = {}
-var scheme = []
-var all = []
-// request[id] = text 
-function generate(childrens){
-  // console.log(childrens)
-  $(childrens).each((index,child)=>{
-    
-    var text = $(child).children('.comment__body ').children('.comment__content').text()
-    var id = $(child).attr('id')
-    // all.push({
-    //   id:id,
-    //   elem:child
-    // })
-    scheme.push({id:id,toxic:0,childrens:[],elem:child})
-
-    request[id] = text
-    if($(child).children('.comment__children')!=0){
-      var childrensCild = $(child).children('.comment__children').children('.comment')
-      // $.each(childrensCild,function(index,item){
-
-      //   scheme[id]['childrens'].push({
-      //     id:$(item).attr('id'),
-      //     elem:item
-      //   })
-      // })
-      generate(childrensCild)
-    }
-    // console.log()
-  })
-
-
-}
-
-
-function sendDev(data){
-  var out = {}
-  for(var elem in data){
-    out[elem] = Number(Math.random().toFixed(2))
-  }
-  return out
+function getRequest() {  
+  var request = {} 
+  $("div.comment:not([data-processed])").map((index, element) => { 
+    var id = $(element).parent().parent().parent().attr("id")  
+    var text = $(element).text() 
+    $(element).attr("data-processed", true)  
+    request[id] = text 
+  }) 
+  return request 
 }
 
 function aggregateToxicity(elements) {
@@ -111,59 +64,24 @@ function stableCompare(e1, e2) {
     return -1
 }
 
-
-
-var oldUrl = null
-// var oldCommentsNumber = 0
-
-// function hideBuggedElements() {
-//   $("div[id^='continueThread']").hide()
-//   $($('div[class ^= Comment]').find('span:contains(Comment deleted by user)')).hide()
-// }
-
-// function getRequest() {  
-//   var request = {} 
-//   $("div[data-test-id=comment]:not([data-processed])").map((index, element) => { 
-//     var id = $(element).parent().parent().parent().attr("id")  
-//     var text = $(element).text() 
-//     $(element).attr("data-processed", true)  
-//     request[id] = text 
-//   }) 
-//   return request 
-// }
-
 function setToxicity(response) {
   Object.keys(response).map((key) => {
     $("#" + key).attr("data-toxicity", response[key])
   })
 
-  // setParent()
-
-  var elements = $('.comments__container').children('.comment').toArray()
+  var elements = $('.comments__container > .comment').toArray()
   aggregateToxicity(elements)
   sortElements(elements)
 }
 
-
-
-// function setParent() {
-//   $("div[data-toxicity] div[id] > div:first-child > div:nth-last-child(2), div[id^='moreComments'] > div:first-child > div:last-child").map((index, element) => {
-//     var parentId = $(element).attr("class").split(" ")[0]
-//     $(element).parent().parent().parent().parent().attr("data-parent-id", parentId)
-//   })
-// }
-
-// function filterToxicComments() {  
-//   hideBuggedElements() 
-//   request = getRequest() 
-//   Toxic.sendToxicityRequest(request, setToxicity)  
-// }
+function filterToxicComments() {  
+  request = getRequest() 
+  Toxic.sendToxicityRequest(request, setToxicity)  
+}
 
 function initSaveToxicityPopups() {
   $('body').on('click', '.set-toxic', function() {
-    // console.log(this)
     var parentPopup = $(this).parent().parent().parent().parent()
-    // var value = $(this).attr('class').split(' ')[1]
     var parentText = $('div[data-target='+$(parentPopup).attr('id')+']')
     var parent = $(parentText).parent().parent()
     
@@ -173,9 +91,7 @@ function initSaveToxicityPopups() {
       toxic: $(parent).attr('data-toxicity'),
       url: location.href
     }
-    console.log(data)
     Toxic.sendSaveToxicityRequest(data, function() {
-      console.log("Thanks!")
       WebuiPopovers.hideAll();
     })
   })
@@ -211,7 +127,7 @@ function checkPage() {
   if (newPageLoaded || (oldCommentsNumber < commentsNumber)) {  
     oldUrl = window.location.href  
     oldCommentsNumber = commentsNumber 
-    // filterToxicComments()
+    filterToxicComments()
     addSaveToxicityPopups()
   }  
   if (newPageLoaded) {
